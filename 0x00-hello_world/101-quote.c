4eb219c662a83c5ce7fc0bb6f70d4e026f04150e
mu9222 <eng92fa64@example.com>
2023-03-18 13:26:11 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd>
+/**
+ * main - A Programm that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
