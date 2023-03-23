90d840bc98967bd79200aef6b4b6c9bbef7b369d
Muhammad Ali <eng92fa64@gmail.com>
2023-03-23 15:07:34 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 'a';
+	while (i <= 'z')
+	{
+	_putchar(i);
+	i++;
+	}
+	_putchar('\n');
+	return (0);
+}
