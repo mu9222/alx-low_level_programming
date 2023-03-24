e317753abd2e40e58be7fe6493971af12339d109
Muhammad Ali <eng92fa64@gmail.com>
2023-03-24 23:16:26 +0000

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,7 @@
 
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_alphabet - print all alphabet in lowercase
  */
 
 void print_alphabet(void)
@@ -13,8 +11,8 @@ void print_alphabet(void)
 
 	while (i <= 'z')
 	{
-	_putchar(i);
-	i++;
+		_putchar(i);
+		i++;
 	}
 	_putchar('\n');
 }
