b5dd984fd5b58d28ce6a84d38e4d420af815cd56
Muhammad Ali <eng92fa64@gmail.com>
2023-03-25 23:16:21 +0000

    Task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	for (i = 1; i <= 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			j = j + i;
+		}
+	}
+	printf("%d ", j);
+	printf("\n");
+	return (0);
+}
