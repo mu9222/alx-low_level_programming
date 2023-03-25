2dafde6ca6a2fd9db02b451a965fe30da1fd766e
Muhammad Ali <eng92fa64@gmail.com>
2023-03-25 18:45:11 +0000

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_sign - function that prints the sign of a number
@@ -10,15 +11,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		return (0);
+		_putchar('0');
+		return (0) ;
 	}
 
 }
