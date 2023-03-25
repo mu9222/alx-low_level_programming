3f19b74d58d3ecae134663d74e149c41c439a43c
Muhammad Ali <eng92fa64@gmail.com>
2023-03-25 22:04:40 +0000

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,19 +13,36 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
 	}
 	else if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d, ", n);
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
 	}
 	else
 	{
 		printf("%d", n);
 	}
-	printf("\n");
 }
