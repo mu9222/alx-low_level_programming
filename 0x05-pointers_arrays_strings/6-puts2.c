fc715be9532d788d68005e42a64397a88fc9945f
Muhammad Ali <eng92fa64@gmail.com>
2023-03-28 23:08:08 +0000

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - function that reverses a string
+ * @str: the pointer
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i; j+= 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
