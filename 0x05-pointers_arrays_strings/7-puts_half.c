de2eee1efe0d746e665a0a13dd175533f5422266
Muhammad Ali <eng92fa64@gmail.com>
2023-03-28 23:33:37 +0000

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,6 +14,7 @@ void puts_half(char *str)
 	{
 		i++;
 	}
+
 	if (i % 2 == 0)
 	{
 		for (j = i / 2; j < i; j++)
@@ -23,7 +24,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (j = (i - 1) / 2; j < i; j++)
+		for (j = ((i - 1) / 2) - 1; j < i; j++)
 		{
 			_putchar(str[j]);
 		}
