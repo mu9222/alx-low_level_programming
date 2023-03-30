f8ac050fc6fc951b0b08cb53e2280e082a1abb88
Muhammad Ali <eng92fa64@gmail.com>
2023-03-30 00:50:26 +0000

    Task 9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - function that prints a square
+ * @size: number of '#'
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
