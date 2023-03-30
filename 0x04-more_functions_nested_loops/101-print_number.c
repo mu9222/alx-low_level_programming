a87fac3679926c9446a762078fc9c75791599ecb
Muhammad Ali <eng92fa64@gmail.com>
2023-03-30 20:52:20 +0000

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer.
+ * @n: integer number
+ */
+
+void print_number(int n)
+{
+	unsigned int l;
+
+	if (n < 0)
+	{
+		l = -n;
+		_putchar('-');
+	}
+	else
+	{
+		l = n;
+	}
+	if (l / 10)
+	{
+		print_number(l / 10);
+	}
+	_putchar((n % 10) + '0');
+}
