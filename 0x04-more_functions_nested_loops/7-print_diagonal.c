a20e8cf161e86d147b97b5d696ae311c9e965d86
Muhammad Ali <eng92fa64@gmail.com>
2023-03-30 00:40:36 +0000

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that prints line
+ * @n: number of '_'
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (j = 0; j < n; j++)
+		{
+			for (i = 0; i <= j; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
