8de7d5dabe301eb4fd7b3701f91725204436a87a
mu9222 <eng92fa64@example.com>
2023-03-31 15:39:30 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,6 +13,7 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
 
 			i++;
 
@@ -31,6 +32,7 @@ char *cap_string(char *str)
 			str[i - 1] == '}' ||
 			i == 0)
 				str[i] -= 32;
+		}
 			i++;
 	}
 	return (str);
