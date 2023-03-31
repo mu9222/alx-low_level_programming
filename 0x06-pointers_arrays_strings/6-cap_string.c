7f1de362fef67b3acd226b82c8804a3022fbe8a7
Muhammad Ali <eng92fa64@gmail.com>
2023-03-31 23:16:14 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,8 +13,7 @@ char *cap_string(char *str)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-<<<<<<< HEAD
-		
+
 			i++;
 
 		if (str[i - 1] == ' ' ||
@@ -33,29 +32,6 @@ char *cap_string(char *str)
 		i == 0)
 			str[i] -= 32;
 		i++;
-=======
-		{
-
-			i++;
-
-			if (str[i - 1] == ' ' ||
-			str[i - 1] == '\t' ||
-			str[i - 1] == '\n' ||
-			str[i - 1] == ',' ||
-			str[i - 1] == ';' ||
-			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
-			str[i - 1] == '?' ||
-			str[i - 1] == '"' ||
-			str[i - 1] == '(' ||
-			str[i - 1] == ')' ||
-			str[i - 1] == '{' ||
-			str[i - 1] == '}' ||
-			i == 0)
-				str[i] -= 32;
-		}
-			i++;
->>>>>>> 8de7d5dabe301eb4fd7b3701f91725204436a87a
 	}
 	return (str);
 }
