985af00a87d0dad68b7ea0e169bce6c0635efa43
Muhammad Ali <eng92fa64@gmail.com>
2023-04-01 01:12:13 +0000

    Task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,15 +7,17 @@
 
 void print_number(int n)
 {
+	unsigned int a;
 
+	a = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		a = -n;
 	}
-	if (n / 10 != 0)
+	if (a / 10 != 0)
 	{
 		print_number(n / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((a % 10) + '0');
 }
