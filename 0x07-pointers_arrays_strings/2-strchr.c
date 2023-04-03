3b21702647c2422df4f9b522cc4844d6c51696ec
Muhammad Ali <eng92fa64@gmail.com>
2023-04-03 22:26:15 +0000

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that  locates a character in a string.
+ * @s: string
+ * @c: character
+ * Return: dest
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int j = 0;
+	char b;
+
+	while (s[i] != '\0')
+	{
+		if ( s[i] == c)
+		{
+			b = s[i];
+			s[j] = b;
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
