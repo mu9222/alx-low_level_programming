cf94cd3c9e64029deaf4f479487855c14f1c277a
Muhammad Ali <eng92fa64@gmail.com>
2023-04-10 20:58:58 +0000

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - function that multiplies two numbers
+ * @argc: number of arguments pass to the main function
+ * @argv: vector of names of arguments
+ * Return: alwayes return (0)
+ **/
+
+
+int main(int argc, char *argv[])
+{
+	int j, k, i = 0;
+
+	if (argc >= 2)
+	{
+		k = 0;
+		for (j = 1; j < argc; j++)
+		{
+			k = atoi(argv[j]);
+			if ( k > 0)
+			{
+				i += atoi(argv[j]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", i);
+	}
+	else if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
