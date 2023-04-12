0958acce8f38527d0050482b7dd4a9c8c5f43085
Muhammad Ali <eng92fa64@gmail.com>
2023-04-12 04:42:40 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,14 +16,14 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int i;
 
-	if (size == 0 || c == '\0')
+	if (size == 0)
 	{
 		return (NULL);
 	}
 	else
 	{
 		str = malloc(sizeof(c) * size);
-		for (i = 0; i < size; i++)
+		for (i = 0; i <= size; i++)
 		{
 			str[i] = c;
 		}
