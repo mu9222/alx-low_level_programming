5f220d3ee4136923092919c9737670a8811328c0
Muhammad Ali <eng92fa64@gmail.com>
2023-04-14 00:23:14 +0000

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * string_nconcat - function that concatenates two strings.
  * @s1: string 1
@@ -8,7 +7,6 @@
  * @n: number of bytes
  * Return: NULL or pointer
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i, j, k, size_s1 = 0, size_s2 = 0;
@@ -25,8 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s1[size_s1] != '\0')
 	{
 		size_s1++;
-	}
-	while (s2[size_s2] != '\0')
+	} while (s2[size_s2] != '\0')
 	{
 		size_s2++;
 	}
@@ -35,21 +32,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (c == NULL)
 	{
 		return (NULL);
-	}
-	else
+	} else if (c != NULL)
 	{
 		for (i = 0; i < size_s1; i++)
 		{
 			c[i] = s1[i];
 		}
-		if (n < size_s2)
-		{
-			for (i = size_s1, k = 0; i < (n + size_s1); i++, k++)
-			{
-				c[i] = s2[k];
-			}
-		}
-		else
+		(n < size_s2) ? (j = size_s1 + n) : (j = size_s1 + size_s2 + 1);
 		{
 			for (i = size_s1, k = 0; i < j; i++, k++)
 			{
