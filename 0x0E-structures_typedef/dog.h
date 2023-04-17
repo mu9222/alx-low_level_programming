37e26e57b43eb91a8871953a43909077c519d7d3
Muhammad Ali <eng92fa64@gmail.com>
2023-04-17 21:33:52 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
