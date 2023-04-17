369ed75e5029290b71ffc996ec0bc038e5f5a4f0
Muhammad Ali <eng92fa64@gmail.com>
2023-04-17 23:38:59 +0000

    Task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+#include "main.h"
+
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: nwe struct
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+		printf("nothing");
+	}
+	if (d->name == NULL)
+		printf("Name: nil");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (d->age <= 0 )
+		printf("nil");
+	else
+		printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("nil");
+	else
+		printf("Owner: %s\n", d->owner);
+}
