3912029d0a9f291cc2840c4685e8edc9d6a2ea2e
Muhammad Ali <eng92fa64@gmail.com>
2023-04-17 23:52:57 +0000

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
-#include "main.h"
 
 
 /**
--- 0x0E-structures_typedef/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef main_h
-#define main_h
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-#endif
