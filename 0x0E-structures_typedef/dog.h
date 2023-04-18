07264dcb1d20985b61879aff1321a517aced0b0b
Muhammad Ali <eng92fa64@gmail.com>
2023-04-18 00:10:33 +0000

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,12 +15,7 @@ struct dog
 	char *owner;
 };
 
-struct dog dog_t
-{
-        char *name;
-        float age;
-        char *owner;
-};
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
