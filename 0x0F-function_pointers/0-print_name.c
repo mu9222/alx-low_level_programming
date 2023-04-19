27839c8470e7798168308f4c99327dc18e9578c4
Muhammad Ali <eng92fa64@gmail.com>
2023-04-19 21:55:16 +0000

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stddef.h>
 
 /**
  * print_name - function that prints a name
@@ -10,5 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
