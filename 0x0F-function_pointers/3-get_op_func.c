43ec8da8aa6baef88ceeb2fdc33ea03764288eb3
Muhammad Ali <eng92fa64@gmail.com>
2023-04-20 05:50:49 +0000

    Task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * get_op_func - function that returns the sum of a and b.
+ * @s: operator
+ * Return: function pointer
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
+}
