37e0fc6ec33739e8cd8ae5b389cdb6690a44327a
Muhammad Ali <eng92fa64@gmail.com>
2023-04-20 15:18:12 +0000

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,6 +20,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		num = va_arg(ap, int);
 
+		if (separator == NULL)
+			printf("%d", num);
 		if (i == (n - 1))
 			printf("%d", num);
 		else
