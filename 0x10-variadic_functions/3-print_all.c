b9fbd02de0a644126f5d4c163b458212442a66b5
Muhammad Ali <eng92fa64@gmail.com>
2023-04-22 02:47:33 +0000

    Task 2

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function that prints strings, followed by a new line.
+ * @separator: separator between elements
+ * @n: number of parameters
+ * Return: Nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *str;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char*);
+
+		if (separator == NULL)
+			printf("%s ", str);
+		if (str == NULL)
+			printf("(nil)");
+		if (i == (n - 1))
+			printf("%s", str);
+		else
+			printf("%s%s", str, separator);
+		va_end(ap);
+	}
+	printf("\n");
+}
