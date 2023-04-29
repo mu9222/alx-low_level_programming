f13edb1aeb3b0b0ecd4b1b69eb97b1eb7e2a919f
Muhammad Ali <eng92fa64@gmail.com>
2023-04-29 22:06:14 +0000

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,13 +3,15 @@
 
 /**
  * main - check the code
+ * @argc: number of arguments
+ * @argv: vector of arguments
  *
  * Return: Always 0.
  */
 
 int main(int argc, char *argv[])
 {
-	unsigned char *ptr = (unsigned char*) main;
+	unsigned char *ptr = (unsigned char *) main;
 	int i, n_bytes;
 
 	if (argc != 2)
@@ -27,7 +29,7 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < n_bytes; i++)
 	{
-		printf("%02x ", *(ptr+i));
+		printf("%02x ", *(ptr + i));
 	}
 	printf("\n");
 	return (0);
