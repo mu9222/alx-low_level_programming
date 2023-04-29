0c03d856fb6881d2df7577bf394c190b614cbfb2
Muhammad Ali <eng92fa64@gmail.com>
2023-04-29 15:41:10 +0000

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,22 +4,21 @@
 #include "lists.h"
 
 /**
- * add_node_end - function that adds a new node at the end of a list_t list.
+ * add_nodeint_end - function that adds a new node at the end of a list_t list.
  * @head: node.
- * @str: string needs to be duplicated.
+ * @n: intiger number
  * Return: the address of the new element, or NULL if it failed.
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	list_t *new_el = malloc(sizeof(list_t));
-	list_t *last_el = malloc(sizeof(list_t));
+	listint_t *new_el = malloc(sizeof(listint_t));
+	listint_t *last_el = malloc(sizeof(listint_t));
 
 	if (new_el == NULL)
 		return (NULL);
 
-	new_el->str = strdup(str);
-	new_el->len = strlen(str);
+	new_el->n = n;
 
 	if (*head == NULL)
 		*head = new_el;
