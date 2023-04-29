7598306aa5e326c1cdd19d9226eb6e739bde954a
Muhammad Ali <eng92fa64@gmail.com>
2023-04-29 14:42:43 +0000

    Task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,11 @@
+#program in assembly that prints Hello, Holberton, followed by a new line
 
-section .data
-    str: db "Hello, Holberton",0
-
-section .text
-    global print_string
-
-print_string:
-    push rbp
-    mov rbp, rsp
-
-    ; call printf to print the string
-    mov rax, 0
-    lea rdi, [rel str]
-    call printf
-
-    ; restore the stack and return
-    mov rsp, rbp
-    pop rbp
-    ret
+	global    main
+	extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
