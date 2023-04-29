8fda9ecc889a6a2be3905b312c61061ffff48c3a
Muhammad Ali <eng92fa64@gmail.com>
2023-04-29 19:19:06 +0000

    Task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - function that returns the number of elements in a list.
+ * @head: point to list
+ * @index: index of node
+ * Return: pointer to node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int n = 0;
+
+	while (head != NULL)
+	{
+		n++;
+		head = head->next;
+
+		if (n == index)
+			return (head);
+	}
+	return (NULL);
+}
