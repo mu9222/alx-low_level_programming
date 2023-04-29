42bff8462a3ab377613a98a73f0b5557202ecf03
Muhammad Ali <eng92fa64@gmail.com>
2023-04-29 12:16:34 +0000

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * frist_excute - function that prints strings befor excute the main function.
+ */
+
+void first_excute()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
