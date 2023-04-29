f039bf19bd6434a75114faf32a67643f2cd051c5
Muhammad Ali <eng92fa64@gmail.com>
2023-04-29 22:02:38 +0000

    Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned char *ptr = (unsigned char*) main;
+	int i, n_bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n_bytes = atoi(argv[1]);
+	if (n_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	for (i = 0; i < n_bytes; i++)
+	{
+		printf("%02x ", *(ptr+i));
+	}
+	printf("\n");
+	return (0);
+}
