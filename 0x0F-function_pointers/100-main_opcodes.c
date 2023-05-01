eafbfce34ec50820d1deaa90a92e44ec3810934e
Muhammad Ali <eng92fa64@gmail.com>
2023-05-01 19:19:13 +0000

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -29,7 +29,12 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < n_bytes; i++)
 	{
-		printf("%02x", *(ptr + i));
+		if (i == n_bytes - 1)
+		{
+			printf("%02x\n", *(ptr + i);
+			break;
+		}
+		printf("%02x ", *(ptr + i));
 	}
 	printf("\n");
 	return (0);
