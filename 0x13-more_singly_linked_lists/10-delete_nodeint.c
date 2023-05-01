df3ba5f3000623abe802feb35a5672405c080f19
Muhammad Ali <eng92fa64@gmail.com>
2023-05-01 20:31:05 +0000

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *curr = NULL;
 	unsigned int n = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 
 	if (index == 0)
