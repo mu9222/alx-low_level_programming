aa524cbbc3aa3a1a0dd1835b751955d50e604d55
Muhammad Ali <eng92fa64@gmail.com>
2023-05-01 20:33:50 +0000

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp;
+	listint_t *temp = *head;
 	listint_t *curr = NULL;
 	unsigned int n = 0;
 
@@ -22,9 +22,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		free(temp);
 		return (1);
 	}
-	temp = *head;
 	while (n < (index - 1))
 	{
 		if (!temp || !(temp->next))
