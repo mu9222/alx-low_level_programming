88b72fa6ad645fe425c03ae01a23b7b3158d6c76
Muhammad Ali <eng92fa64@gmail.com>
2023-05-06 12:55:58 +0000

    Task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 void rev_string(char *s);
+int _putchar(char);
+
 #endif
